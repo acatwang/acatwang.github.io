c9c6e4451a2500d612eb7a4a5cd1fd0b5a769995
YYwang <acatwang@gmail.com>
2014-11-06 21:24:35 -0500

    add slide div

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -13,3 +13,13 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
